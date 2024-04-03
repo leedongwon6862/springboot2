456a04055c9ff9f9afea67462de1f7d731e97512
kimjh1234 <ran824200@gmail.com>
2024-04-03 17:18:45 +0900

    2024-04-03

---
+++ jumptos/src/main/java/com/example/jumptos/JumptosApplication.java
@@ -0,0 +1,13 @@
+package com.example.jumptos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumptosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JumptosApplication.class, args);
+	}
+
+}
---
+++ jumptos/src/test/java/com/example/jumptos/JumptosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jumptos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumptosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
